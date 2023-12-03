be476f5e26515a246767984bcc0aa6eb2c21a598
kapil <upadhyaykapil61@gmail.com>
2023-12-03 19:41:08 +0530

    folder structure for backened and frontend and A home page is also set up

---
+++ client/src/App.jsx
@@ -0,0 +1,33 @@
+import {BrowserRouter , Link , Route , Routes} from "react-router-dom"
+import { logo } from "./assets";
+import Home  from "./pages/Home"
+import CreatePost from "./pages/CreatePost";
+
+
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+  <BrowserRouter>
+  <header className="w-full  flex justify-between items-center bg-white sm:px-8 px-4 border-b-[#e6ebf4]" >
+    <Link to = "/">
+      <img src={logo} alt="logo" className="w-28 object-contain"/>
+    </Link>
+    <Link to = "/createpost" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create</Link>
+  </header>
+  <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/createpost" element={<CreatePost/>}/>
+    </Routes>
+  </main>
+
+
+  </BrowserRouter>
+  )
+}
+
+export default App
