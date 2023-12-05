93bab2831c65d6875edcc4f81aadf5631b39631b
kapil <upadhyaykapil61@gmail.com>
2023-12-05 22:22:28 +0530

    a component formfield is made

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 
-export default function FormField() {
+export default function FormField({LabelName,type,
+name,
+placeholder,
+value,
+handleChange,
+isSurpriseMe,
+handleSurpriseMe}) {
   return (
     <div>
-      he
+      <div className='flex items-center gap-2 mb-2'>
+        <label
+        htmlFor={name}
+        className='block text-sm font-medium text-gray-900'>
+          {LabelName}</label>
+          {isSurpriseMe && (
+            <button type='button' onClick={handleSurpriseMe} className='font-semibold text-xs bg-[#ECECF1] px-1 py-2 rounded-[5px] text-black'>Surprise Me</button>
+          )}
+      </div>
+      <input type={type}
+      name={name}
+      id={name}
+      placeholder={placeholder}
+      value={value}
+      onChange={handleChange}
+      required
+      className='bg-gray-50  border bg-gray-300 text-gray-900 text-sm-rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3'></input>
     </div>
   )
 }
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -18,6 +18,12 @@ export default function CreatePost() {
   const handleSubmit = ()=>{
 
   }
+  const handleChange=(e)=>{
+
+  }
+  const handleSurpriseMe=()=>{
+
+  }
   return (
     <section className='max-w-7xl mx-auto'>
       <div>
@@ -26,7 +32,42 @@ export default function CreatePost() {
       </div>
       <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
         <div className='flex flex-col gap-5'>
+        <FormField
+        LabelName="Your name"
+        type="text"
+        name="name"
+        placeholder="kapil"
+        value={form.name}
+        handleChange={handleChange}/>
 
+        <FormField
+        LabelName="Prompt"
+        type="text"
+        name="prompt"
+        placeholder=  'an armchair in the shape of an avocado'
+        value={form.prompt}
+        handleChange={handleChange}
+        isSurpriseMe
+        handleSurpriseMe={handleSurpriseMe}/>
+
+        <div className='relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+        focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex-justify-center items-center'>
+          {form.photo?(
+            <img src={form.photo}
+            alt={form.prompt}
+            className='w-full h-full object-contain'/>
+          ):(
+            <img src={preview}
+            alt="preview"
+            className='w-9/12 h-9/12 object-contain opacity-40'/>
+          )}
+          {generatingImg && (
+            <div className='absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5) rounded-lg'>
+              <Loader/>
+            </div>
+
+          )}
+        </div>
         </div>
       </form>
     </section>
