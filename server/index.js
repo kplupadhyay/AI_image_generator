edaf257105cb5aa89e785a68ce9abb2324ec9e6b
kapil <upadhyaykapil61@gmail.com>
2023-12-22 22:27:54 +0530

    A portion of backend in completed along with routing

--- server/index.js
+++ server/index.js
@@ -1,15 +1,33 @@
-import express from "express"
-import * as dotenv from "dotenv"
-import cors from "cors"
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
 
-dotenv.config()
+import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
+
+dotenv.config();
+
 const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }));
 
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
 
-app.listen(3600 );
+app.get('/', async (req, res) => {
+  res.status(200).json({
+    message: 'Hello from DALL.E!',
+  });
+});
 
-app.get("/" , (req,res)=>{
-    res.send("hello I am AI image generator")
-})
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () => console.log('Server started on port 8080'));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
